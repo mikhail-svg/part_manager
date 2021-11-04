b00ebe5be2093c80629c0d2a268fcc5071ae42e8
mikhail-svg <80486745+mikhail-svg@users.noreply.github.com>
2021-11-04 16:58:14 +0300

    Repurposed
    Renamed variable references and db references to match current application requirements.

--- db.py
+++ db.py
@@ -6,37 +6,31 @@ class Database:
         self.conn = sqlite3.connect(db)
         self.cur = self.conn.cursor()
         self.cur.execute(
-            "CREATE TABLE IF NOT EXISTS parts (id INTEGER PRIMARY KEY, part text, customer text, retailer text, price text)")
+            "CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, student_name text, student_age text, homeroom_teacher text, avg_grade text)")
         self.conn.commit()
 
     def fetch(self):
-        self.cur.execute("SELECT * FROM parts")
+        self.cur.execute("SELECT * FROM students")
         rows = self.cur.fetchall()
         return rows
 
-    def insert(self, part, customer, retailer, price):
-        self.cur.execute("INSERT INTO parts VALUES (NULL, ?, ?, ?, ?)",
-                         (part, customer, retailer, price))
+    def insert(self, student_name, student_age, homeroom_teacher, avg_grade):
+        self.cur.execute("INSERT INTO students VALUES (NULL, ?, ?, ?, ?)",
+                         (student_name, student_age, homeroom_teacher, avg_grade))
         self.conn.commit()
 
     def remove(self, id):
-        self.cur.execute("DELETE FROM parts WHERE id=?", (id,))
+        self.cur.execute("DELETE FROM students WHERE id=?", (id,))
         self.conn.commit()
 
-    def update(self, id, part, customer, retailer, price):
-        self.cur.execute("UPDATE parts SET part = ?, customer = ?, retailer = ?, price = ? WHERE id = ?",
-                         (part, customer, retailer, price, id))
+    def update(self, id, student_name, student_age, homeroom_teacher, avg_grade):
+        self.cur.execute("UPDATE students SET part = ?, customer = ?, retailer = ?, price = ? WHERE id = ?",
+                         (student_name, student_age, homeroom_teacher, avg_grade, id))
         self.conn.commit()
 
     def __del__(self):
         self.conn.close()
 
 
-# db = Database('store.db')
-# db.insert("4GB DDR4 Ram", "John Doe", "Microcenter", "160")
-# db.insert("Asus Mobo", "Mike Henry", "Microcenter", "360")
-# db.insert("500w PSU", "Karen Johnson", "Newegg", "80")
-# db.insert("2GB DDR4 Ram", "Karen Johnson", "Newegg", "70")
-# db.insert("24 inch Samsung Monitor", "Sam Smith", "Best Buy", "180")
-# db.insert("NVIDIA RTX 2080", "Albert Kingston", "Newegg", "679")
-# db.insert("600w Corsair PSU", "Karen Johnson", "Newegg", "130")
+db = Database('students.db')
+
--- part_manager_oop.py
+++ part_manager_oop.py
@@ -3,7 +3,7 @@ from tkinter import messagebox
 from db import Database
 
 # Instanciate databse object
-db = Database('store.db')
+db = Database('students.db')
 
 # Main Application/GUI class
 
@@ -12,7 +12,7 @@ class Application(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
         self.master = master
-        master.title('Part Manager')
+        master.title('Student Manager')
         # Width height
         master.geometry("700x350")
         # Create widgets/grid
@@ -24,61 +24,61 @@ class Application(tk.Frame):
 
     def create_widgets(self):
         # Part
-        self.part_text = tk.StringVar()
-        self.part_label = tk.Label(
-            self.master, text='Part Name', font=('bold', 14), pady=20)
-        self.part_label.grid(row=0, column=0, sticky=tk.W)
-        self.part_entry = tk.Entry(self.master, textvariable=self.part_text)
-        self.part_entry.grid(row=0, column=1)
+        self.name_text = tk.StringVar()
+        self.name_label = tk.Label(
+            self.master, text='Student Name', font=('bold', 14), pady=20)
+        self.name_label.grid(row=0, column=0, sticky=tk.W, padx=5)
+        self.name_entry = tk.Entry(self.master, textvariable=self.name_text)
+        self.name_entry.grid(row=0, column=1)
         # Customer
-        self.customer_text = tk.StringVar()
-        self.customer_label = tk.Label(
-            self.master, text='Customer', font=('bold', 14))
-        self.customer_label.grid(row=0, column=2, sticky=tk.W)
-        self.customer_entry = tk.Entry(
-            self.master, textvariable=self.customer_text)
-        self.customer_entry.grid(row=0, column=3)
+        self.age_text = tk.StringVar()
+        self.age_label = tk.Label(
+            self.master, text='Student Age', font=('bold', 14))
+        self.age_label.grid(row=0, column=2, sticky=tk.W)
+        self.age_entry = tk.Entry(
+            self.master, textvariable=self.age_text)
+        self.age_entry.grid(row=0, column=3)
         # Retailer
-        self.retailer_text = tk.StringVar()
-        self.retailer_label = tk.Label(
-            self.master, text='Retailer', font=('bold', 14))
-        self.retailer_label.grid(row=1, column=0, sticky=tk.W)
-        self.retailer_entry = tk.Entry(
-            self.master, textvariable=self.retailer_text)
-        self.retailer_entry.grid(row=1, column=1)
+        self.homeroom_teacher_text = tk.StringVar()
+        self.homeroom_teacher_label = tk.Label(
+            self.master, text='Homeroom Teacher', font=('bold', 14))
+        self.homeroom_teacher_label.grid(row=1, column=0, sticky=tk.W, padx = 5)
+        self.homeroom_teacher_entry = tk.Entry(
+            self.master, textvariable=self.homeroom_teacher_text)
+        self.homeroom_teacher_entry.grid(row=1, column=1)
         # Price
-        self.price_text = tk.StringVar()
-        self.price_label = tk.Label(
-            self.master, text='Price', font=('bold', 14))
-        self.price_label.grid(row=1, column=2, sticky=tk.W)
-        self.price_entry = tk.Entry(self.master, textvariable=self.price_text)
-        self.price_entry.grid(row=1, column=3)
+        self.avg_grade_text = tk.StringVar()
+        self.avg_grade_label = tk.Label(
+            self.master, text='Average Grade', font=('bold', 14))
+        self.avg_grade_label.grid(row=1, column=2, sticky=tk.W)
+        self.avg_grade_entry = tk.Entry(self.master, textvariable=self.avg_grade_text)
+        self.avg_grade_entry.grid(row=1, column=3)
 
         # Parts list (listbox)
-        self.parts_list = tk.Listbox(self.master, height=8, width=50, border=0)
-        self.parts_list.grid(row=3, column=0, columnspan=3,
+        self.names_list = tk.Listbox(self.master, height=8, width=100, border=1)
+        self.names_list.grid(row=3, column=0, columnspan=4,
                              rowspan=6, pady=20, padx=20)
         # Create scrollbar
         self.scrollbar = tk.Scrollbar(self.master)
-        self.scrollbar.grid(row=3, column=3)
-        # Set scrollbar to parts
-        self.parts_list.configure(yscrollcommand=self.scrollbar.set)
-        self.scrollbar.configure(command=self.parts_list.yview)
+        self.scrollbar.grid(row=3, column=4)
+        # Set scrollbar to names
+        self.names_list.configure(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.configure(command=self.names_list.yview)
 
         # Bind select
-        self.parts_list.bind('<<ListboxSelect>>', self.select_item)
+        self.names_list.bind('<<ListboxSelect>>', self.select_item)
 
         # Buttons
         self.add_btn = tk.Button(
-            self.master, text="Add Part", width=12, command=self.add_item)
+            self.master, text="Add Student", width=12, command=self.add_item)
         self.add_btn.grid(row=2, column=0, pady=20)
 
         self.remove_btn = tk.Button(
-            self.master, text="Remove Part", width=12, command=self.remove_item)
+            self.master, text="Remove Student", width=12, command=self.remove_item)
         self.remove_btn.grid(row=2, column=1)
 
         self.update_btn = tk.Button(
-            self.master, text="Update Part", width=12, command=self.update_item)
+            self.master, text="Update Student", width=12, command=self.update_item)
         self.update_btn.grid(row=2, column=2)
 
         self.exit_btn = tk.Button(
@@ -87,27 +87,26 @@ class Application(tk.Frame):
 
     def populate_list(self):
         # Delete items before update. So when you keep pressing it doesnt keep getting (show example by calling this twice)
-        self.parts_list.delete(0, tk.END)
+        self.names_list.delete(0, tk.END)
         # Loop through records
         for row in db.fetch():
             # Insert into list
-            self.parts_list.insert(tk.END, row)
+            self.names_list.insert(tk.END, row)
 
     # Add new item
     def add_item(self):
-        if self.part_text.get() == '' or self.customer_text.get() == '' or self.retailer_text.get() == '' or self.price_text.get() == '':
+        if self.name_text.get() == '' or self.age_text.get() == '' or self.homeroom_teacher_text.get() == '' or self.avg_grade_text.get() == '':
             messagebox.showerror(
                 "Required Fields", "Please include all fields")
             return
-        print(self.part_text.get())
         # Insert into DB
-        db.insert(self.part_text.get(), self.customer_text.get(),
-                  self.retailer_text.get(), self.price_text.get())
+        db.insert(self.name_text.get(), self.age_text.get(),
+                  self.homeroom_teacher_text.get(), self.avg_grade_text.get())
         # Clear list
-        self.parts_list.delete(0, tk.END)
+        self.names_list.delete(0, tk.END)
         # Insert into list
-        self.parts_list.insert(tk.END, (self.part_text.get(), self.customer_text.get(
-        ), self.retailer_text.get(), self.price_text.get()))
+        self.names_list.insert(tk.END, (self.name_text.get(), self.age_text.get(
+        ), self.homeroom_teacher_text.get(), self.avg_grade_text.get()))
         self.clear_text()
         self.populate_list()
 
@@ -117,20 +116,20 @@ class Application(tk.Frame):
         # global self.selected_item
         try:
             # Get index
-            index = self.parts_list.curselection()[0]
+            index = self.names_list.curselection()[0]
             # Get selected item
-            self.selected_item = self.parts_list.get(index)
+            self.selected_item = self.names_list.get(index)
             # print(selected_item) # Print tuple
 
             # Add text to entries
-            self.part_entry.delete(0, tk.END)
-            self.part_entry.insert(tk.END, self.selected_item[1])
-            self.customer_entry.delete(0, tk.END)
-            self.customer_entry.insert(tk.END, self.selected_item[2])
-            self.retailer_entry.delete(0, tk.END)
-            self.retailer_entry.insert(tk.END, self.selected_item[3])
-            self.price_entry.delete(0, tk.END)
-            self.price_entry.insert(tk.END, self.selected_item[4])
+            self.name_entry.delete(0, tk.END)
+            self.name_entry.insert(tk.END, self.selected_item[1])
+            self.age_entry.delete(0, tk.END)
+            self.age_entry.insert(tk.END, self.selected_item[2])
+            self.homeroom_teacher_entry.delete(0, tk.END)
+            self.homeroom_teacher_entry.insert(tk.END, self.selected_item[3])
+            self.avg_grade_entry.delete(0, tk.END)
+            self.avg_grade_entry.insert(tk.END, self.selected_item[4])
         except IndexError:
             pass
 
@@ -142,16 +141,16 @@ class Application(tk.Frame):
 
     # Update item
     def update_item(self):
-        db.update(self.selected_item[0], self.part_text.get(
-        ), self.customer_text.get(), self.retailer_text.get(), self.price_text.get())
+        db.update(self.selected_item[0], self.name_text.get(
+        ), self.age_text.get(), self.homeroom_teacher_text.get(), self.avg_grade_text.get())
         self.populate_list()
 
     # Clear all text fields
     def clear_text(self):
-        self.part_entry.delete(0, tk.END)
-        self.customer_entry.delete(0, tk.END)
-        self.retailer_entry.delete(0, tk.END)
-        self.price_entry.delete(0, tk.END)
+        self.name_entry.delete(0, tk.END)
+        self.age_entry.delete(0, tk.END)
+        self.homeroom_teacher_entry.delete(0, tk.END)
+        self.avg_grade_entry.delete(0, tk.END)
 
 
 root = tk.Tk()
